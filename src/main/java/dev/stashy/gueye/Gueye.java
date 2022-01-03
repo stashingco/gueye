1e5ab50a4e97e523263b3e656c5ebc671f15f9c4
stashymane <aestheticrice@gmail.com>
2022-01-03 16:17:06 +0200

    init

---
+++ src/main/java/dev/stashy/gueye/Gueye.java
@@ -0,0 +1,12 @@
+package dev.stashy.gueye;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Gueye implements ModInitializer
+{
+    @Override
+    public void onInitialize()
+    {
+
+    }
+}
